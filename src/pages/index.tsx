88a4935092a2c88655a0246a695e699329684438
Takuya Hirata <takuyahirata4@gmail.com>
2021-03-22 08:42:59 -0400

    WIP: create theme

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,17 @@
-import { Box } from "@material-ui/core";
+import Image from 'next/image'
+import { AppBar, Box } from '@material-ui/core'
+
 export default function Home() {
-  return <Box>hello</Box>;
+  return (
+    <Box bgcolor="background.default">
+      <AppBar component="header" color="transparent">
+        <Image
+          src="/slack-logo.svg"
+          alt="Picture of the author"
+          width={150}
+          height={80}
+        />
+      </AppBar>
+    </Box>
+  )
 }
---
+++ src/utils/theme.ts
@@ -0,0 +1,14 @@
+import { createMuiTheme } from '@material-ui/core/styles'
+
+export const lightTheme = createMuiTheme({
+  palette: {
+    text: {
+      primary: '#1d1d1d',
+      secondary: '#ffffff'
+    },
+    background: {
+      default: '#4a154b',
+      paper: '#f6efe8'
+    }
+  }
+})
